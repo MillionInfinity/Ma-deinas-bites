f6072b5677e857c1fb7c1dfb83f771c2efdf164a
Million Yemane <wedi_ngus@yahoo.com>
2018-04-06 01:35:49 -0500

    changed the all cookies size

--- js/main.js
+++ js/main.js
@@ -5,32 +5,71 @@
 
 
          
-        $('#cover').click(function () {
-            $('#cover').attr('src', 'imgs/u5.jpg');
-        });
+        // $('#ab').click(function () {
+        //     $('#ab').attr('src', 'imgs/parm-qv.png');
+        // });
+
 
 $(function () {
-    $(".img-swap").live('click', function () {
-        if ($(this).attr("class") == "img-swap") {
-            this.src = this.src.replace("_off", "_on");
-        } else {
-            this.src = this.src.replace("_on", "_off");
+    $("#ab").on({
+        mouseenter: function () {
+            $(this).attr('src', 'imgs/parm-qv.png');
+        },
+        mouseleave: function () {
+            $(this).attr('src', 'imgs/parm.png');
         }
-        $(this).toggleClass("on");
     });
+
+});
+
+$(function () {
+    $("#bc").on({
+        mouseenter: function () {
+            $(this).attr('src', 'imgs/oatmeal_bites-qv.png');
+        },
+        mouseleave: function () {
+            $(this).attr('src', 'imgs/oatmeal_bites.png');
+        }
+    });
+
+});
+
+$(function () {
+    $("#cd").on({
+        mouseenter: function () {
+            $(this).attr('src', 'imgs/jam_poppy-qv.png');
+        },
+        mouseleave: function () {
+            $(this).attr('src', 'imgs/jam_poppy.png');
+        }
+    });
+
+});
+
+$("#background").click(function(){
+    window.location="shop.html";
+});
+
+$("#shop").click(function () {
+    window.location = "shop.html";
 });
 
 
 
+//for collapse button item.html
 
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-
-
-
-// $("#bought").click(function () {
-//     $("#welcome").remove();
-//     $(".uniContainer-wrapper").html("");
-//     loadBoughtBookToDOM();
-//     meg.purple();
-// });
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
